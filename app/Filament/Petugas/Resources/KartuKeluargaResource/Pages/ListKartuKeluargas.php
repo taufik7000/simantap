b5bee9bfa07be178ef36949f428b7168610f486f
taufik7000 <mtaufikxxx@gmail.com>
2025-07-01 04:43:29 +0700

    fix minor styling

--- app/Filament/Petugas/Resources/KartuKeluargaResource/Pages/ListKartuKeluargas.php
+++ app/Filament/Petugas/Resources/KartuKeluargaResource/Pages/ListKartuKeluargas.php
@@ -13,7 +13,6 @@ class ListKartuKeluargas extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
         ];
     }
 }
