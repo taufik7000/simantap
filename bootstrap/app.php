6593aea6f680d56ec8b436b5c8c46286a8cd4ff8
taufik7000 <mtaufikxxx@gmail.com>
2025-07-02 14:33:16 +0700

    fix: jika user sudah login direct ke halaman dahsboard sesuai role

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class, // Tambahkan baris ini
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
