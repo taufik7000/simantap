3d05016e230b2327d4d3439f66dac900c3cae196
taufik7000 <mtaufikxxx@gmail.com>
2025-07-07 04:29:19 +0700

    minor

--- app/Providers/Filament/PetugasPanelProvider.php
+++ app/Providers/Filament/PetugasPanelProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers\Filament;
 
-use App\Filament\Petugas\Resources\PermohonanResource;
-use Filament\Navigation\NavigationItem;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -36,17 +34,8 @@ class PetugasPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Emerald,
             ])
-            ->navigationItems([
-                NavigationItem::make('Permohonan Terverifikasi')
-                    ->label('Permohonan Terverifikasi')
-                    // Pastikan label ini sama persis dengan 'navigationLabel' di PermohonanResource.php
-                    ->parentItem('Permohonan Warga') 
-                    ->icon('heroicon-o-check-badge')
-                    ->url('/petugas/permohonans?tableFilters[status][values][0]=proses_entri&tableFilters[status][values][1]=entri_data_selesai&tableFilters[status][values][2]=menunggu_persetujuan&tableFilters[status][values][3]=disetujui&tableFilters[status][values][4]=dokumen_diterbitkan&tableFilters[status][values][5]=proses_pengiriman&tableFilters[status][values][6]=selesai')
-                    ->sort(1),
-            ])
             ->databaseNotifications()
-            ->databaseNotificationsPolling('5s')
+            ->databaseNotificationsPolling('30s')
             ->discoverResources(in: app_path('Filament/Petugas/Resources'), for: 'App\\Filament\\Petugas\\Resources')
             ->discoverPages(in: app_path('Filament/Petugas/Pages'), for: 'App\\Filament\\Petugas\\Pages')
             ->discoverWidgets(in: app_path('Filament/Petugas/Widgets'), for: 'App\\Filament\\Petugas\\Widgets')
